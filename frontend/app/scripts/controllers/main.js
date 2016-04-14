5a574ec8d845c16a6f27d56a98744298ac9ee2b0
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-14 13:17:54 -0430

    Añadiendo el frontend en angular generado por yeoman

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsletterFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newsletterFrontendApp
+ */
+angular.module('newsletterFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
