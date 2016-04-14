11caab59706501ff3cd806965323e9654d0da8ca
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-14 11:26:01 -0430

    Implementando el método de envio masivo de correos

--- common/models/recipient.js
+++ common/models/recipient.js
@@ -4,6 +4,8 @@ var aws = require('aws-sdk');
 var credentials = new aws.SharedIniFileCredentials({profile: 'default'});
 aws.config.update({region: 'us-west-2'});
 var ses = new aws.SES({apiVersion: '2010-12-01'});
+var _ = require('lodash');
+var Q = require('q');
 
 module.exports = function(Recipient) {
   Recipient.afterRemote('create', function (context, recipient, next) {
@@ -20,5 +22,50 @@ module.exports = function(Recipient) {
       console.log(data);           // successful response
       next(null);
     });
+  });
+
+  Recipient.send = function (subject, body, cb) {
+    ses.listVerifiedEmailAddresses().promise()
+    .then(function(data) {
+      return data.VerifiedEmailAddresses
+    })
+    .then(function(emailsList){
+      var from = 'alexis.ibarra@ultra.sur.top'
+
+      return Q.all(
+        _.map(emailsList, function(address){
+          return ses.sendEmail({
+            Source: from,
+            Destination: { ToAddresses: [ address ] },
+            Message: {
+              Subject: {
+                Data: subject
+              },
+              Body: {
+                Text: {
+                  Data: body,
+                }
+              }
+            }
+          }).promise()
+        })
+      );
+    })
+    .then(function(responses){
+      cb(null, responses);
+    })
+    .catch(function(error){
+      cb(error);
+    });
+  };
+
+
+  Recipient.remoteMethod('send', {
+    accepts: [
+      {arg: 'subject', type: 'string', 'http': {source: 'body'} },
+      {arg: 'body', type: 'string', 'http': {source: 'body'} },
+    ],
+    returns: {root: true, type: 'Object'},
+    http: {path: '/send', verb: 'post'}
   });
 };
