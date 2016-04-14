66c17a61d573128a0354a13866d8588edbae5335
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-14 16:44:48 -0430

    Implementando logica para envio de correos y registro de usuarios

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,10 +8,42 @@
  * Controller of the newsletterFrontendApp
  */
 angular.module('newsletterFrontendApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+      $scope.backendURL = 'http://localhost:3000/api/Recipients/';
+
+      $scope.registerUser = function () {
+          console.log($scope.backendURL);
+          $http({
+              method: 'POST',
+              url: $scope.backendURL,
+              data:{
+                  "address": $scope.email,
+                  "name": $scope.name
+              }
+          })
+              .then(function(res){
+                  console.log(res);
+              })
+              .catch(function(err){
+                  console.log(err);
+              });
+      };
+
+      $scope.registerUser = function () {
+          $http({
+              method: 'POST',
+              url: $scope.backendURL + 'send',
+              data:{
+                  "subject": $scope.subject,
+                  "content": $scope.content
+              }
+          })
+              .then(function(res){
+                  console.log(res);
+              })
+              .catch(function(err){
+                  console.log(err);
+              });
+      }
+      
   });
