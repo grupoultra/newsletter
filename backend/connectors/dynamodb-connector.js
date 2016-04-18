9e9e2d9526f08e236a51e861fd612366d4a97ffa
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-18 17:31:03 -0430

    reimplemtando las rutinas para update, all y create

--- backend/connectors/dynamodb-connector.js
+++ backend/connectors/dynamodb-connector.js
@@ -1,4 +1,19 @@
 var Connector = require('loopback-connector').Connector;
+var config = require('../server/config.json');
+var aws = require('aws-sdk');
+var _ = require('lodash');
+
+
+aws.config.update({
+    region: config.region,
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secretAccessKey
+});
+
+var dynamodb = new aws.DynamoDB();
+var docClient = new aws.DynamoDB.DocumentClient();
+// On top of your script, or whatever
+
 
 
 /**
@@ -21,23 +36,91 @@ exports.initialize = function initializeDataSource(dataSource, callback) {
  * @constructor
  */
 function DynamoDB(settings, dataSource) {
-    Connector.call(this, 'cloudinary', settings);
+    Connector.call(this, 'dynamodb', settings);
     this.dataSource = dataSource;
 }
+
+DynamoDB.prototype.update = function updateAll(model, where, data, options, cb){
+    var params = {
+        TableName: "ultra-newsletter",
+        Key:{
+            "token": where.token
+        },
+        UpdateExpression: "set verified = :T",
+        ExpressionAttributeValues: {
+            ":T": true
+        }
+    };
+
+    docClient.update(params, function(err, data) {
+        if (err) {
+            cb(err);
+            // console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            cb(null, data);
+            // console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+        }
+    });
+};
 
 /**
  * Create a new model instance
  */
-DynamoDB.prototype.create = function (model, data, callback) {
-    console.log(model, data);
-    // callback(err, err ? null);
+DynamoDB.prototype.create = function (model, data, cb) {
+    var params = {
+        TableName: "ultra-newsletter",
+        Item: {
+            "token": data.token,
+            "address":  data.address,
+            "campaign": "new-prueba",
+            "verified": false,
+            "fullname": data.fullname
+            // TODO cambiar la campana
+        }
+    };
 
+    docClient.put(params, function(err, datae) {
+        if (err) {
+            cb(err);
+        } else {
+            cb(null, datae);
+        }
+    });
+};
+
+
+/**
+ * Find all model instances
+ */
+DynamoDB.prototype.all = function (model, filter, options, cb) {
+    var params = {
+        TableName: "ultra-newsletter",
+        ProjectionExpression: "address, fullname",
+        FilterExpression: "verified = :verified",
+        ExpressionAttributeValues: {
+            ":verified": true
+        }
+    };
+
+    if(filter.verified){
+        params["ExpressionAttributeValues"][":verified"] = filter.verified;
+    }
+
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.error("Error JSON:", JSON.stringify(err, null, 2));
+            cb(err);
+        } else {
+            cb(null, data.Items);
+        }
+    });
 };
 
 /**
  * Save a model instance
  */
-DynamoDB.prototype.save = function (model, data, callback) {
+DynamoDB.prototype.save = function (model, data, cb) {
+
     console.log(model, data);
     // callback(err, err ? null);
 };
