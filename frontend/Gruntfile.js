e51840e1751b45bce8361b24fae953d46fa81e64
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-26 17:05:07 -0430

    Actualizando el URI del api

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -436,7 +436,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'development',
-            apiEndpoint: 'http://localhost:3000/api'
+            apiEndpoint: 'https://9zrt55mz44.execute-api.us-east-1.amazonaws.com/beta'
           }
         }
       },
