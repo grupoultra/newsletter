0cec4c46186d7851c05f469c667f6786dda7c1df
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-26 17:50:40 -0430

    Anadiendo controlador de verificacion con su vista

---
+++ frontend/app/scripts/controllers/verification.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('newsletterFrontendApp')
+  .controller('VerificationCtrl', function ($scope, $routeParams, $http) {
+    var token = $routeParams.token;
+
+    $scope.verify = function(){
+      $http({
+        method: 'POST',
+        url: $scope.backendURL,
+        data:{
+          "operation": "verify",
+          "token": token
+        },
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        }
+      })
+      .then(function(res){
+        console.log("lo que sea");
+      });
+    };
+  });
