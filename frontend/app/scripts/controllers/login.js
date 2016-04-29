415920ff4df6b74b1f9f647c764931b54e86f3b4
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-04-29 17:39:25 -0430

    haciendo lo ajustes necesarios para comunicarse con el backend y manejar las respuestas

--- frontend/app/scripts/controllers/login.js
+++ frontend/app/scripts/controllers/login.js
@@ -9,24 +9,33 @@
  */
 angular.module('newsletterFrontendApp')
     .controller('LoginCtrl', function ($scope, $http, ENV, store) {
-        $scope.backendURL = ENV.apiEndpoint + "/Clients/";
+        $scope.backendURL = ENV.apiEndpoint + "/users/";
         $scope.isLoggedIn = function(){
-            var auth_token = store.get('authentication_token');
+            var auth_token = store.get('Authorization');
 
             return !( auth_token === null || auth_token === undefined );
-        }
+        };
 
         $scope.login = function (email, password) {
             $http({
                 method: 'POST',
-                url: $scope.backendURL + "login",
+                url: $scope.backendURL,
                 data:{
+                    "operation": "login",
                     "username": $scope.username,
                     "password": $scope.password
                 }
             })
             .then(function(res){
-                store.set('authentication_token', res.data.id);
+                if(res.data.errorMessage){
+                  $scope.errorMessage = res.data.errorMessage;
+                  $scope.error = true;
+                  $scope.success = false;
+                } else{
+                  $scope.success = true;
+                  $scope.error = false;
+                  store.set('Authorization', res.data.id);
+                }
                 console.log(res);
             })
             .catch(function(err){
@@ -35,6 +44,8 @@ angular.module('newsletterFrontendApp')
         }
 
         $scope.logout = function () {
-            store.remove('authentication_token');
+          $scope.error = false;
+          $scope.success = false;
+            store.remove('Authorization');
         }
     });
